c5834cccdf04b7467eb0c7ab1837990f6e2236ad
Ashubh01 <shubhamdhokare01@gmail.com>
2024-06-20 22:28:54 +0530

    Fixed postId issue in read page

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -13,8 +13,6 @@ export function SideBar({toggleSideBar}: ToggleSideBarType){
     const setDraftBlogData = useSetRecoilState(draftBlogDataAtom);
 
     function cleanUp(){
-        localStorage.removeItem('postId');
-        setDraftBlogData({id: '', title: '', content: ''});
         toggleSideBar();
     }
 
@@ -35,6 +33,7 @@ export function SideBar({toggleSideBar}: ToggleSideBarType){
                 <MenuItem logo={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>}
                 title="New Blog" onClickEvent={() => {
                     cleanUp();
+                    setDraftBlogData({id: '', title: '', content: ''});
                     navigate('/write');
                 }}/>
             </div>
